9841c31cea6877b986f07ad1551fc490d2b69290
geomydas <155147963+geomydas@users.noreply.github.com>
2024-11-03 21:21:18 +0800

    refactored js

--- src/script.js
+++ src/script.js
@@ -1,23 +1,41 @@
 const form = document.querySelector("#form");
 const inputEmail = document.querySelector("#email");
 
-function handleSubmit(e) {
-  e.preventDefault();
+function handleSubmit(event) {
+  event.preventDefault();
 }
 
-function validateInput(input) {
-  if (input.validity.valid) {
+function inputValid(input) {
+  input.classList.add("outline-paleblue");
+  input.classList.remove("outline-red");
+  input.setAttribute("aria-invalid", "false");
+  input.nextElementSibling.classList.add("invisible");
+  input.nextElementSibling.classList.remove("visible");
+  input.nextElementSibling.textContent = "";
+}
+
+function inputInvalid(input, message) {
+  input.classList.add("outline-red");
+  input.classList.remove("outline-paleblue");
+  input.setAttribute("aria-invalid", "true");
+  input.nextElementSibling.classList.add("visible");
+  input.nextElementSibling.classList.remove("invisible");
+  input.nextElementSibling.textContent = message;
+}
+
+function validateEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const validateEmail = emailRegex.test(email.value);
+
+  if (validateEmail) {
+    inputValid(email);
+    form.reset();
+  } else {
+    inputInvalid(email, "Please enter a valid email address");
   }
 }
 
-function resetForm(form) {
-  form.reset();
-}
-
-form.addEventListener("submit", function (e) {
-  handleSubmit(e);
-
-  if (inputEmail.validity.valid) {
-    resetForm(form);
-  }
+form.addEventListener("submit", function (event) {
+  handleSubmit(event);
+  validateEmail(inputEmail);
 });
